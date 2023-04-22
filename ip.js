1d6783a0c8e2aa8f4a1429ec776181ba1639be2b
ellidaan <elone.meimoun@epitech.digital>
2023-04-22 19:02:35 +0200

    zezez

--- ip.js
+++ ip.js
@@ -1,9 +1,4 @@
 const express = require('express');
-const { MongoClient} = require('mongodb')
-const url ='mongodb+srv://ellidaanx:Mot2passe@ip.7wkf38t.mongodb.net/test'
-const dbName = 'ip';
-const collectionName = 'ips';
-
 
 
 const app = express();
@@ -11,31 +6,9 @@ const app = express();
 
 
 app.get('/', (req, res) => {
-  const ip = req.socket.remoteAddress;
-  res.json(`Votre adresse IP publique est : ${ip}`);
-});
-
-
-
-
-app.get('/i', async(req, res) => {
-  try { 
-     const client = new MongoClient(url);
-    const db = client.db(dbName);
-    const collection = db.collection(collectionName);
-  
- 
   const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    const ips = collection.insertOne({ip: req.socket.remoteAddress});
-  res.json(`Votre adresse IP publique est : ${ip}`);
-  }
-  catch (err) {
-    console.error(err);
-    res.status(500).json({ message: 'Internal server error' });
-  }
-
+  res.send(`Votre adresse IP publique est : ${ip}`);
 });
-
 
 
 

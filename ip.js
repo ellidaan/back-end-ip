adf67105505e92bfc47aea456d0c382c91b18b51
ellidaan <elone.meimoun@epitech.digital>
2023-04-22 19:24:04 +0200

    aaa# Please enter the commit message for your changes. Lines starting

--- ip.js
+++ ip.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
-
+const { MongoClient} = require('mongodb')
+const url ='mongodb+srv://ellidaanx:Mot2passe@ip.7wkf38t.mongodb.net/test'
+const dbName = 'ip';
+const collectionName = 'ips';
 
 
 app.get('/', (req, res) => {
+  const client = new MongoClient(url);
+  const db = client.db(dbName);
+  const collection = db.collection(collectionName);
+  const ips= collection.insertOne({ip: req.headers['x-forwarded-for'] || req.socket.remoteAddress})
   const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
   res.send(`Votre adresse IP publique est : ${ip}`);
 });

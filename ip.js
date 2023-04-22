cd6e92a3ce0c8c81d6fef7bfd8f640ef3784d455
ellidaan <elone.meimoun@epitech.digital>
2023-04-22 16:05:00 +0200

    testtt

--- ip.js
+++ ip.js
@@ -1,45 +1,20 @@
 const express = require('express');
-const http = require('http');
+const request = require('request');
 
 const app = express();
-const { MongoClient} = require('mongodb');
-const cors = require('cors'); // Importer le module cors
-app.use(cors())
-
-
-const url ="mongodb+srv://ellidaanx:Mot2passe@ip.7wkf38t.mongodb.net/test";
-const dbName = "ip";
-const collectionName = "ips";
-const client = new MongoClient(url);
-
+const port = 3000;
 
 app.get('/', (req, res) => {
-    http.get('http://api.ipify.org', (resp) => {
-      resp.on('data', async (ip) => {
-        try {
-            await client.connect();
-            const db = client.db(dbName);
-            const collection = db.collection(collectionName);
-          const result = await collection.insertOne({
-            ip: ip.toString(),
-            date: new Date().toISOString()
-          });
-          res.json(`Your public IP address is: ${ip}`);
-        
-        } catch (err) {
-          console.error(err);
-          res.status(500).json({ message: 'Internal server error' });
-        } finally {
-          await client.close();
-        }
-      });
-    }).on("error", (err) => {
-      console.log("Error: " + err.message);
-      res.status(500).send("An error occurred while fetching the IP address.");
-    });
+  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  const url = `https://api.ipgeolocation.io/ipgeo?apiKey=131da5aae3fb47a2a8e5f64ac6c8c330&ip=${ip}`;
+
+  request(url, { json: true }, (err, response, body) => {
+    if (err) { return console.log(err); }
+
+    console.log(body);
+    res.send(body);
   });
-
-
+});
 
 
 

57e44a5e39561fa593c88628c4140e187813f852
ellidaan <elone.meimoun@epitech.digital>
2023-04-22 23:39:05 +0200

    aze

--- ip.js
+++ ip.js
@@ -11,15 +11,20 @@ app.get('/', async (req, res) => {
   const client = new MongoClient(url);
   const db = client.db(dbName);
   const collection = db.collection(collectionName);
-  const ip =  req.socket.remoteAddress;
+  const forwarded = req.headers['x-forwarded-for'];
+  const ip = forwarded ? forwarded.split(',')[0] : req.socket.remoteAddress;
+  
   const existingIp = await collection.findOne({ ip: ip }); // Vérifie si l'adresse IP est déjà présente dans la collection
   if (existingIp) {
     console.log(`IP ${ip} already exists in the collection`);
-    res.send(existingIp.ip);
+    res.send(ip);
+    console.log(ip);
   } else {
     const newIp = await collection.insertOne({ ip: ip });
     console.log(`Inserted new IP ${ip} into the collection`);
   }
+  client.close();
+    
 });
 
 

4be25dabc7e09d06b6c8a8553209473fd250a76f
ellidaan <elone.meimoun@epitech.digital>
2023-04-22 16:20:18 +0200

    testt

--- ip.js
+++ ip.js
@@ -1,19 +1,13 @@
 const express = require('express');
-const request = require('request');
+
 
 const app = express();
-const port = 3000;
+
+
 
 app.get('/', (req, res) => {
   const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-  const url = `https://api.ipgeolocation.io/ipgeo?apiKey=131da5aae3fb47a2a8e5f64ac6c8c330&ip=${ip}`;
-
-  request(url, { json: true }, (err, response, body) => {
-    if (err) { return console.log(err); }
-
-    console.log(body);
-    res.send(body);
-  });
+  res.send(`Votre adresse IP publique est : ${ip}`);
 });
 
 
